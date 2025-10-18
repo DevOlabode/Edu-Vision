91bdf289a289c0c2d45e3dab9fb8690e3a717a68
Samuel Olabode <Solabode499@gmail.com>
2025-10-18 16:33:28 -0600

    `Added initial auth controller, user model, and route for registration form`

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    firstName : {
+        type : String,
+        required : true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    bio : {
+        type : String,
+        required : false
+    },
+    role : {
+        type : String,
+        enum : ['Teacher' , 'Student'],
+        required : true
+    }
+}, { timestamps: true }
+);
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+
+module.exports = mongoose.model('User', userSchema);

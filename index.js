4ce83c32ce3e088cf531f8ce58c617aaa0d13032
Samuel Olabode <Solabode499@gmail.com>
2025-10-18 20:05:21 -0600

    `Added navbar and flash partials to boilerplate layout, removed welcome message from home route, and added login route to auth routes.`

--- index.js
+++ index.js
@@ -73,7 +73,6 @@ mongoose.connect(process.env.MONGO_URL)
 app.use('/', authRoutes);    
 
 app.get('/', (req, res)=>{
-    req.flash('success', 'Welcome to EduVision AI');
     res.render('home');
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,9 @@ const catchAsync = require('../utils/catchAsync');
 const userController = require('../controllers/auth');
 
 router.get('/register', userController.registerForm);
+
 router.post('/register', catchAsync(userController.register));
+
+router.get('/login', userController.loginForm);
+
 module.exports = router;
